7b5d2f23cfc6d88fdf1932fb15a994b19402c763
Fizik05 <bboybaga13@mail.ru>
2022-07-04 15:06:41 +0300

    My second work-commit

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -22,7 +22,7 @@ if settings.DEBUG:
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
     urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+        settings.STATIC_URL, document_root=settings.STATICFILES_DIRS
     )
     import debug_toolbar
 
